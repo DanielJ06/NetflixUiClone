88e6b5719cb2fcb9d18f5ce2cbf19e849c6a70f2
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-02-05 16:36:38 -0300

    URL request for dynamic data

--- app/src/main/java/com/danieljrodrigues/netflixclone/MainActivity.java
+++ app/src/main/java/com/danieljrodrigues/netflixclone/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 
 import com.danieljrodrigues.netflixclone.model.Category;
 import com.danieljrodrigues.netflixclone.model.Movie;
+import com.danieljrodrigues.netflixclone.utils.JsonDownloadTask;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
         );
         CategoryAdapter adapter = new CategoryAdapter(categories);
         categoryRecyclerView.setAdapter(adapter);
+
+        new JsonDownloadTask(MainActivity.this).execute("https://tiagoaguiar.co/api/netflix/home");
     }
 
     private class MovieHolder extends RecyclerView.ViewHolder {
